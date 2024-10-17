6c0f7d23b708952bb2b20df091ca6db7e939cec9
pelcp <s95521@pollub.edu.pl>
2024-10-17 08:40:50 +0200

    bigfix1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello world!");
+        System.out.println("cos");
+        System.out.println("123");
+        System.out.println("321");
+        System.out.println("dobrze");
     }
 }
